2eccca53bbda77150ef75de336544128143d129d
heshi0816 <917909657@qq.com>
2024-06-01 15:28:09 +0800

    ini

---
+++ src/main/java/com/dream/nusblog/NusblogApplication.java
@@ -0,0 +1,13 @@
+package com.dream.nusblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NusblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NusblogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dream/nusblog/NusblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dream.nusblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NusblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
