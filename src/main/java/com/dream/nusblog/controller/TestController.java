673d8ee759dce925f083cf7496e332a682a6e0c1
heshi0816 <23heshi@gmail.com>
2024-06-02 11:37:31 +0800

    test http

--- src/main/java/com/dream/nusblog/controller/TestController.java
+++ src/main/java/com/dream/nusblog/controller/TestController.java
@@ -2,6 +2,7 @@ package com.dream.nusblog.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +11,10 @@ public class TestController {
     @GetMapping("/hello")
     public String hello() {
         return "hello world";
+    }
+
+    @PostMapping("/hello/post")
+    public String helloPost(String name) {
+        return "hello world post " + name;
     }
 }
