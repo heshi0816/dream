448f33ef712fa2444f7d3b8e220074628693ac1b
heshi0816 <23heshi@gmail.com>
2024-06-02 09:07:18 +0800

    controller

---
+++ src/main/java/com/dream/nusblog/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.dream.nusblog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Controller
+public class TestController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello world";
+    }
+}
