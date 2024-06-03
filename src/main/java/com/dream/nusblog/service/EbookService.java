99351a78adf2b11515bc63aae77c3b2343983125
heshi0816 <23heshi@gmail.com>
2024-06-03 18:10:05 +0800

    EbookService Optimization

--- src/main/java/com/dream/nusblog/service/EbookService.java
+++ src/main/java/com/dream/nusblog/service/EbookService.java
@@ -5,11 +5,10 @@ import com.dream.nusblog.domain.EbookExample;
 import com.dream.nusblog.mapper.EbookMapper;
 import com.dream.nusblog.req.EbookReq;
 import com.dream.nusblog.resp.EbookResp;
-import org.springframework.beans.BeanUtils;
+import com.dream.nusblog.util.CopyUtil;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -24,13 +23,8 @@ public class EbookService {
         criteria.andNameLike("%" + req.getName() + "%");
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
 
-        List<EbookResp> respList = new ArrayList<>();
-        for (Ebook ebook : ebookList) {
-            EbookResp ebookResp = new EbookResp();
-            BeanUtils.copyProperties(ebook, ebookResp);
-            respList.add(ebookResp);
-        }
+        List<EbookResp> list = CopyUtil.copyList(ebookList, EbookResp.class);
 
-        return respList;
+        return list;
     }
 }
