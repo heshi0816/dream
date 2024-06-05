59b69dbeb9adffeb98c4af28d6b1cd36ccc54fea
heshi0816 <23heshi@gmail.com>
2024-06-05 11:50:24 +0800

    5.4 add ant-design vue list

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,75 +1,125 @@
 <template>
-  <a-layout>
-    <a-layout-sider width="200" style="background: #fff">
-      <a-menu
-              mode="inline"
-              :style="{ height: '100%', borderRight: 0 }"
-      >
-        <a-sub-menu key="sub1">
-          <template #title>
-            <span><user-outlined />subnav 11111</span>
-          </template>
-          <a-menu-item key="1">option1</a-menu-item>
-          <a-menu-item key="2">option2</a-menu-item>
-          <a-menu-item key="3">option3</a-menu-item>
-          <a-menu-item key="4">option4</a-menu-item>
-        </a-sub-menu>
-        <a-sub-menu key="sub2">
-          <template #title>
-            <span><laptop-outlined />subnav 2</span>
-          </template>
-          <a-menu-item key="5">option5</a-menu-item>
-          <a-menu-item key="6">option6</a-menu-item>
-          <a-menu-item key="7">option7</a-menu-item>
-          <a-menu-item key="8">option8</a-menu-item>
-        </a-sub-menu>
-        <a-sub-menu key="sub3">
-          <template #title>
-            <span><notification-outlined />subnav 3</span>
-          </template>
-          <a-menu-item key="9">option9</a-menu-item>
-          <a-menu-item key="10">option10</a-menu-item>
-          <a-menu-item key="11">option11</a-menu-item>
-          <a-menu-item key="12">option12</a-menu-item>
-        </a-sub-menu>
-      </a-menu>
-    </a-layout-sider>
-    <a-layout-content
-            :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
-    >
-      <pre>
-{{ebooks}}
-{{ebooks2}}
-      </pre>
-    </a-layout-content>
-  </a-layout>
+    <a-layout>
+        <a-layout-sider width="200" style="background: #fff">
+            <a-menu
+                    mode="inline"
+                    :style="{ height: '100%', borderRight: 0 }"
+            >
+                <a-sub-menu key="sub1">
+                    <template #title>
+                        <span><user-outlined />subnav 11111</span>
+                    </template>
+                    <a-menu-item key="1">option1</a-menu-item>
+                    <a-menu-item key="2">option2</a-menu-item>
+                    <a-menu-item key="3">option3</a-menu-item>
+                    <a-menu-item key="4">option4</a-menu-item>
+                </a-sub-menu>
+                <a-sub-menu key="sub2">
+                    <template #title>
+                        <span><laptop-outlined />subnav 2</span>
+                    </template>
+                    <a-menu-item key="5">option5</a-menu-item>
+                    <a-menu-item key="6">option6</a-menu-item>
+                    <a-menu-item key="7">option7</a-menu-item>
+                    <a-menu-item key="8">option8</a-menu-item>
+                </a-sub-menu>
+                <a-sub-menu key="sub3">
+                    <template #title>
+                        <span><notification-outlined />subnav 3</span>
+                    </template>
+                    <a-menu-item key="9">option9</a-menu-item>
+                    <a-menu-item key="10">option10</a-menu-item>
+                    <a-menu-item key="11">option11</a-menu-item>
+                    <a-menu-item key="12">option12</a-menu-item>
+                </a-sub-menu>
+            </a-menu>
+        </a-layout-sider>
+        <a-layout-content
+                :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
+        >
+            <a-list item-layout="vertical" size="large" :pagination="pagination" :data-source="listData">
+                <template #footer>
+                    <div><b>ant design vue</b> footer part</div>
+                </template>
+                <template #renderItem="{ item }">
+                    <a-list-item key="item.title">
+                        <template #actions>
+          <span v-for="{ type, text } in actions" :key="type">
+            <component v-bind:is="type" style="margin-right: 8px" />
+            {{ text }}
+          </span>
+                        </template>
+                        <template #extra>
+                            <img
+                                    width="272"
+                                    alt="logo"
+                                    src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
+                            />
+                        </template>
+                        <a-list-item-meta :description="item.description">
+                            <template #title>
+                                <a :href="item.href">{{ item.title }}</a>
+                            </template>
+                            <template #avatar><a-avatar :src="item.avatar"/></template>
+                        </a-list-item-meta>
+                        {{ item.content }}
+                    </a-list-item>
+                </template>
+            </a-list>
+        </a-layout-content>
+    </a-layout>
 </template>
 
 <script lang="ts">
-  import { defineComponent, onMounted, ref, reactive, toRef } from 'vue';
-  import axios from 'axios';
+    import { defineComponent, onMounted, ref, reactive, toRef } from 'vue';
+    import axios from 'axios';
 
-  export default defineComponent({
-    name: 'Home',
-    setup() {
-      console.log("setup");
-      const ebooks = ref();
-      const ebooks1 = reactive({books: []});
-
-      onMounted(() => {
-        console.log("onMounted");
-        axios.get("http://127.0.0.1:8880/ebook/list?name=Spring").then((response) => {
-          const data = response.data;
-          ebooks.value = data.content;
-          ebooks1.books = data.content;
-          console.log(response);
+    const listData: any = [];
+    for (let i = 0; i < 23; i++) {
+        listData.push({
+            href: 'https://www.antdv.com/',
+            title: `ant design vue part ${i}`,
+            avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+            description:
+                'Ant Design, a design language for background applications, is refined by Ant UED Team.',
+            content:
+                'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
         });
-      });
-
-      return {
-        ebooks,
-        ebooks2: toRef(ebooks1, "books")
-      }
     }
-  });
+
+    export default defineComponent({
+        name: 'Home',
+        setup() {
+            console.log("setup");
+            const ebooks = ref();
+            const ebooks1 = reactive({books: []});
+
+            onMounted(() => {
+                console.log("onMounted");
+                axios.get("http://127.0.0.1:8880/ebook/list?name=Spring").then((response) => {
+                    const data = response.data;
+                    ebooks.value = data.content;
+                    ebooks1.books = data.content;
+                    console.log(response);
+                });
+            });
+
+            return {
+                ebooks,
+                ebooks2: toRef(ebooks1, "books"),
+                listData,
+                pagination: {
+                    onChange: (page: any) => {
+                        console.log(page);
+                    },
+                    pageSize: 3,
+                },
+                actions: [
+                    { type: 'StarOutlined', text: '156' },
+                    { type: 'LikeOutlined', text: '156' },
+                    { type: 'MessageOutlined', text: '2' },
+                ],
+            }
+        }
+    });
 </script>
