e5e73706921ac08a38d1d4b71a16166d638a9b7b
heshi0816 <23heshi@gmail.com>
2024-06-09 11:59:56 +0800

    7.3 category backend and frontend

---
+++ src/main/java/com/dream/nusblog/req/CategoryQueryReq.java
@@ -0,0 +1,8 @@
+package com.dream.nusblog.req;
+
+public class CategoryQueryReq extends PageReq {
+    @Override
+    public String toString() {
+        return "CategoryQueryReq{} " + super.toString();
+    }
+}
---
+++ src/main/java/com/dream/nusblog/service/CategoryService.java
@@ -0,0 +1,81 @@
+package com.dream.nusblog.service;
+
+import com.dream.nusblog.domain.Category;
+import com.dream.nusblog.domain.CategoryExample;
+import com.dream.nusblog.mapper.CategoryMapper;
+import com.dream.nusblog.req.CategoryQueryReq;
+import com.dream.nusblog.req.CategorySaveReq;
+import com.dream.nusblog.resp.CategoryQueryResp;
+import com.dream.nusblog.resp.PageResp;
+import com.dream.nusblog.util.CopyUtil;
+import com.dream.nusblog.util.SnowFlake;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class CategoryService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(CategoryService.class);
+
+    @Resource
+    private CategoryMapper categoryMapper;
+
+    @Resource
+    private SnowFlake snowFlake;
+
+    public PageResp<CategoryQueryResp> list(CategoryQueryReq req) {
+        CategoryExample categoryExample = new CategoryExample();
+        CategoryExample.Criteria criteria = categoryExample.createCriteria();
+        PageHelper.startPage(req.getPage(), req.getSize());
+        List<Category> categoryList = categoryMapper.selectByExample(categoryExample);
+
+        PageInfo<Category> pageInfo = new PageInfo<>(categoryList);
+        LOG.info("总行数：{}", pageInfo.getTotal());
+        LOG.info("总页数：{}", pageInfo.getPages());
+
+        // List<CategoryResp> respList = new ArrayList<>();
+        // for (Category category : categoryList) {
+        //     // CategoryResp categoryResp = new CategoryResp();
+        //     // BeanUtils.copyProperties(category, categoryResp);
+        //     // 对象复制
+        //     CategoryResp categoryResp = CopyUtil.copy(category, CategoryResp.class);
+        //
+        //     respList.add(categoryResp);
+        // }
+
+        // 列表复制
+        List<CategoryQueryResp> list = CopyUtil.copyList(categoryList, CategoryQueryResp.class);
+
+        PageResp<CategoryQueryResp> pageResp = new PageResp();
+        pageResp.setTotal(pageInfo.getTotal());
+        pageResp.setList(list);
+
+        return pageResp;
+    }
+
+    /**
+     * 保存
+     */
+    public void save(CategorySaveReq req) {
+        Category category = CopyUtil.copy(req, Category.class);
+        if (ObjectUtils.isEmpty(req.getId())) {
+            // 新增
+            category.setId(snowFlake.nextId());
+            categoryMapper.insert(category);
+        } else {
+            // 更新
+            categoryMapper.updateByPrimaryKey(category);
+        }
+    }
+
+    public void delete(Long id) {
+        categoryMapper.deleteByPrimaryKey(id);
+    }
+}
