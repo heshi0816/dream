3fa9c12c365de525cfd4f7787c267ba990fedf1a
heshi0816 <23heshi@gmail.com>
2024-06-11 22:29:04 +0800

    8.11 add doc-content retrieve function

--- src/main/java/com/dream/nusblog/service/DocService.java
+++ src/main/java/com/dream/nusblog/service/DocService.java
@@ -111,5 +111,10 @@ public class DocService {
         criteria.andIdIn(ids);
         docMapper.deleteByExample(docExample);
     }
+
+    public String findContent(Long id) {
+        Content content = contentMapper.selectByPrimaryKey(id);
+        return content.getContent();
+    }
 }
 
