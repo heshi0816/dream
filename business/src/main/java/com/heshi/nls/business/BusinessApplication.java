19d3cea30cd6af81e68c21791b05d326d4582998
heshi0816 <23heshi@gmail.com>
2024-06-24 19:45:41 +0800

    add http testing api

--- business/src/main/java/com/heshi/nls/business/BusinessApplication.java
+++ business/src/main/java/com/heshi/nls/business/BusinessApplication.java
@@ -2,12 +2,19 @@ package com.heshi.nls.business;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.ConfigurableEnvironment;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @SpringBootApplication
 public class BusinessApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BusinessApplication.class, args);
+        ConfigurableEnvironment environment = SpringApplication.run(BusinessApplication.class, args).getEnvironment();
+        log.info("启动成功！");
+        log.info("测试地址：http://localhost:{}{}/hello"
+                , environment.getProperty("server.port")
+                , environment.getProperty("server.servlet.context-path"));
     }
 
 }
