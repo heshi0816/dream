c2020f63de85f2b1fc9a98887a0e693dcb28bbec
heshi0816 <23heshi@gmail.com>
2024-06-25 21:24:40 +0800

    using example type to implement query method

--- business/src/main/java/com/heshi/nls/business/controller/TestController.java
+++ business/src/main/java/com/heshi/nls/business/controller/TestController.java
@@ -1,9 +1,12 @@
 package com.heshi.nls.business.controller;
 
+import com.heshi.nls.business.domain.Demo;
 import com.heshi.nls.business.service.DemoService;
 import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class TestController {
@@ -18,5 +21,10 @@ public class TestController {
     @GetMapping("/count")
     public int count() {
         return demoService.count();
+    }
+
+    @GetMapping("/query")
+    public List<Demo> query(String mobile) {
+        return demoService.query(mobile);
     }
 }
--- business/src/main/java/com/heshi/nls/business/service/DemoService.java
+++ business/src/main/java/com/heshi/nls/business/service/DemoService.java
@@ -1,9 +1,13 @@
 package com.heshi.nls.business.service;
 
+import com.heshi.nls.business.domain.Demo;
+import com.heshi.nls.business.domain.DemoExample;
 import com.heshi.nls.business.mapper.DemoMapper;
 import com.heshi.nls.business.mapper.cust.DemoMapperCust;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class DemoService {
@@ -14,7 +18,18 @@ public class DemoService {
     private DemoMapper demoMapper;
 
     public int count() {
-        // return demoMapperCust.count();
-        return Math.toIntExact(demoMapper.countByExample(null));
+        return demoMapperCust.count();
+        // return Math.toIntExact(demoMapper.countByExample(null));
+    }
+
+    public List<Demo> query(String mobile) {
+        DemoExample demoExample = new DemoExample();
+        demoExample.setOrderByClause("id asc");
+        DemoExample.Criteria criteria = demoExample.createCriteria();
+        if (mobile != null) {
+            criteria.andMobileEqualTo(mobile);
+        }
+        List<Demo> list = demoMapper.selectByExample(demoExample);
+        return list;
     }
 }
