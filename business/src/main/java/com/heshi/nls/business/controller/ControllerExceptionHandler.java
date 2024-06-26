df874597f0547294a02579330c80eed51535f70f
heshi0816 <23heshi@gmail.com>
2024-06-26 10:52:06 +0800

    integrate validation annotation

--- business/src/main/java/com/heshi/nls/business/controller/ControllerExceptionHandler.java
+++ business/src/main/java/com/heshi/nls/business/controller/ControllerExceptionHandler.java
@@ -3,6 +3,7 @@ package com.heshi.nls.business.controller;
 import com.heshi.nls.business.exception.BusinessException;
 import com.heshi.nls.business.resp.CommonResp;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -33,6 +34,16 @@ public class ControllerExceptionHandler {
         log.error("系统异常：", e);
         commonResp.setSuccess(false);
         commonResp.setMessage(e.getE().getDesc());
+        return commonResp;
+    }
+
+    @ExceptionHandler(value = BindException.class)
+    @ResponseBody
+    public CommonResp<Object> exceptionHandler(BindException e) {
+        CommonResp<Object> commonResp = new CommonResp<>();
+        log.error("校验异常：{}", e.getBindingResult().getAllErrors().get(0).getDefaultMessage());
+        commonResp.setSuccess(false);
+        commonResp.setMessage(e.getBindingResult().getAllErrors().get(0).getDefaultMessage());
         return commonResp;
     }
 
