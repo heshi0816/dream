b71a5bcf19602c8f3bd2c5de6480304a9abce729
heshi0816 <23heshi@gmail.com>
2024-07-02 00:19:18 +0800

    use h5 session to make cache of the information to solve the data loss problem after refreshing

--- web/src/store/index.js
+++ web/src/store/index.js
@@ -1,12 +1,16 @@
 import { createStore } from 'vuex'
 
+const MEMBER = "member";
+
 export default createStore({
     state: {
-        member: {},
+        member: window.SessionStorage.get(MEMBER) || {},
     },
     mutations: {
         setMember (state, _member) {
             state.member = _member;
+            window.SessionStorage.set(MEMBER, _member);
         }
     },
 })
+
