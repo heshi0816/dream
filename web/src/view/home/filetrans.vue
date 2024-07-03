e513c49cbf7e35bbcf7d508ca19c6f8d70eb89ec
heshi0816 <23heshi@gmail.com>
2024-07-03 16:14:39 +0800

    add uploading audio bottom

---
+++ web/src/view/home/filetrans.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <a-button type="primary" @click="showModal">开始上传音频</a-button>
+    <filetrans-upload ref="filetransUploadCom"></filetrans-upload>
+  </div>
+</template>
+<script setup>
+import FiletransUpload from "./filetrans-upload.vue.vue";
+import {ref} from "vue";
+
+const filetransUploadCom = ref();
+
+const showModal = () => {
+  filetransUploadCom.value.showModal();
+};
+
+</script>
