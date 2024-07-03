62cad4a158229fa4bef9fd739b66ab723210391e
heshi0816 <23heshi@gmail.com>
2024-07-03 23:33:28 +0800

    add filetran table and generate the corresponding type

---
+++ business/src/main/java/com/heshi/nls/business/mapper/FiletransMapper.java
@@ -0,0 +1,30 @@
+package com.heshi.nls.business.mapper;
+
+import com.heshi.nls.business.domain.Filetrans;
+import com.heshi.nls.business.domain.FiletransExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface FiletransMapper {
+    long countByExample(FiletransExample example);
+
+    int deleteByExample(FiletransExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Filetrans record);
+
+    int insertSelective(Filetrans record);
+
+    List<Filetrans> selectByExample(FiletransExample example);
+
+    Filetrans selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") Filetrans record, @Param("example") FiletransExample example);
+
+    int updateByExample(@Param("record") Filetrans record, @Param("example") FiletransExample example);
+
+    int updateByPrimaryKeySelective(Filetrans record);
+
+    int updateByPrimaryKey(Filetrans record);
+}
